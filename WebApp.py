a153377c9d7743fac087f0ffa7ec0b17b4c7cde9
sjdwyer <48137578+sjdwyer@users.noreply.github.com>
2019-03-10 11:30:08 +0000

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -16,4 +16,4 @@ def status():
                       })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080, debug=True) #Running on localhost/port 8080
+    app.run(host='0.0.0.0',port=5000, debug=True) #Running on localhost/port 8080
